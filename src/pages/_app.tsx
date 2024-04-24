b3f0148e44498ea6b793ef27d05fb3bd50f98e74
Rafael Freire <rafaeldacostafreire@gmail.com>
2024-04-24 11:08:25 -0300

    fix: :bug: removing a ';'

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,7 @@ export default function App({Component, pageProps}: AppProps) {
   return (
     <>
       <GlobalStyles />
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </>
   );
 }
